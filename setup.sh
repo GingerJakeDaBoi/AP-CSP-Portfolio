df2a697394a4c2eb92de516c294fd8eb54dec486
GingerJakeDaBoi <GingerJakeDaBoi@users.noreply.github.com>
2022-04-08 21:24:04 -0400

    Initial setup outline

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Root access required..."
+
+#TODO: Check for root access
+
+echo "Setup will create the required daemons and copy the required scripts for the application to work."
+echo "Press any key to continue."
+read
+
+#TODO: More scripts.
+
+###################################
+# EXAMPLE
+# echo "Installing telemetric service"
+# bash scripts/telemetrics
+###################################
