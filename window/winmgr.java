f8b86736273d4b018e06b0bb4fee8aca9edf8631
GingerJake <uniquereddy90@gmail.com>
2022-04-13 08:13:16 -0400

    rename winmgr.java

--- window/winmgr.java
+++
@@ -1,9 +0,0 @@
-Jframe frame = new JFrame("Main window")
-
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
-frame.pack();
-
-frame.setVisible(true);
