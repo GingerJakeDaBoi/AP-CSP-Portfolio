fde782165bb2840f199fca6e59a80157c2ad22c6
GingerJake <uniquereddy90@gmail.com>
2022-04-13 08:41:19 -0400

    Working window drawn

--- window/Project.java
+++ window/Project.java
@@ -1,12 +1,22 @@
-import javax.swing.JFrame;
+package components;
 
-public class Project{
-    public static void main(string[] args){
-        class topLevelWindow{
-            public void createWindow(){
-                JFrame frame = new JFrame("Server");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            }
-        }
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Project {
+
+    public static void main(String[] args) {
+        // Create and set up the window.
+        JFrame frame = new JFrame("MainWindow");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel resources = new JLabel("System Resources");
+        resources.setPreferredSize(new Dimension(800, 600));
+        frame.getContentPane().add(resources, BorderLayout.CENTER);
+
+        // Display the window.
+        frame.pack();
+        frame.setVisible(true);
     }
 }
