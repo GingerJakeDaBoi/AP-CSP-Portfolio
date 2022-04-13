d78db50e9899f33f87ec6e08cd9c3126268280fb
GingerJake <uniquereddy90@gmail.com>
2022-04-13 08:13:01 -0400

    Window start

---
+++ window/Project.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Project{
+    public static void main(string[] args){
+        class topLevelWindow{
+            public void createWindow(){
+                JFrame frame = new JFrame("Server");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            }
+        }
+    }
+}
