eef7e6c5d420f57f30eeb09715dc5db1aea0be26
GingerJakeDaBoi <GingerJakeDaBoi@users.noreply.github.com>
2022-04-14 19:58:11 -0400

    delete terminal once again (not going to use it)

--- gui/Project.java
+++ gui/Project.java
@@ -5,23 +5,23 @@ public class Project {
 
     public static void main(String[] args) {
         // Create and set up the window.
-        JFrame frame = new JFrame("Server Manager");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame ManagerWindow = new JFrame("Server Manager");
+        ManagerWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //System Resources text
         JLabel resources = new JLabel();
         resources.setText("System Resources");
-        frame.getContentPane().add(resources, BorderLayout.CENTER);
+        ManagerWindow.getContentPane().add(resources, BorderLayout.CENTER);
 
         //Terminal text
         JLabel terminal = new JLabel();
         terminal.setText("Terminal");
-        frame.getContentPane().add(terminal,BorderLayout.NORTH);
+        ManagerWindow.getContentPane().add(terminal,BorderLayout.NORTH);
 
         //Filesystem Text
         JLabel Filesystem = new JLabel();
         Filesystem.setText("Filesystem");
-        frame.getContentPane().add(Filesystem,BorderLayout.SOUTH);
+        ManagerWindow.getContentPane().add(Filesystem,BorderLayout.SOUTH);
 
         //Set the sizes of the tabs
         resources.setPreferredSize(new Dimension(800, 400));
@@ -29,7 +29,7 @@ public class Project {
         Filesystem.setPreferredSize(new Dimension(800,400));
         
         // Display the window.
-        frame.pack();
-        frame.setVisible(true);
+        ManagerWindow.pack();
+        ManagerWindow.setVisible(true);
     }
 }
