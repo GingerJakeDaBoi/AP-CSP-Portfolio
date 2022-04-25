621a9fa5a52d039cbba4c33617d78558fe8bdf0b
GingerJakeDaBoi <GingerJakeDaBoi@users.noreply.github.com>
2022-04-25 16:15:26 -0400

    add cpu arch identifier in the corner

--- src/main/java/org/apcsp/portfolio/StartScreen.java
+++ src/main/java/org/apcsp/portfolio/StartScreen.java
@@ -4,13 +4,16 @@ import java.awt.*;
 import java.io.File;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryManagerMXBean;
 import java.lang.management.OperatingSystemMXBean;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.text.AttributedCharacterIterator;
 import java.text.MessageFormat;
 import javax.swing.*;
 
 public class StartScreen extends ServerState {
-    OperatingSystemMXBean operatingSystemMXBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+    OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
     private final String[] LABELS={"System Resources","Process List"};
 
     public StartScreen(ServerStateManager ssm){
@@ -24,33 +27,33 @@ public class StartScreen extends ServerState {
     public void draw(Graphics g) {
         //Set the background color
         g.setColor(new Color(93, 93, 93, 255));
-        g.fillRect(0,0,ServerPanel.width,ServerPanel.height);
+        g.fillRect(0, 0, ServerPanel.width, ServerPanel.height);
 
         //Draw the labels, look at the top for the list
-        for(int i = 0; i < LABELS.length; i++) {
+        for (int i = 0; i < LABELS.length; i++) {
             g.setColor(Color.white);
 
             //Category Labels
-            g.setFont(new Font("Arial",Font.BOLD,16));
-            g.drawString(LABELS[i],25,80+i*225);
+            g.setFont(new Font("Arial", Font.BOLD, 16));
+            g.drawString(LABELS[i], 25, 80 + i * 225);
 
             //OS name and version
-            g.setFont(new Font("Arial",Font.PLAIN,12));
-            g.drawString(System.getProperty("os.name"),10,20);
-            g.drawString(System.getProperty("os.version"),10,40);
+            g.setFont(new Font("Arial", Font.PLAIN, 12));
+            g.drawString(System.getProperty("os.name"), 10, 20);
+            g.drawString(System.getProperty("os.version"), 10, 40);
+            g.drawString(System.getProperty("os.arch"),755,20);
 
             // CPU usage
-            g.setFont(new Font("Arial",Font.PLAIN,16));
-            g.drawString("CPU Count: "+ String.valueOf(Runtime.getRuntime().availableProcessors()),25,105);
-            g.drawString("Average CPU Usage: "+String.valueOf(MessageFormat.format("{0}%", operatingSystemMXBean.getSystemLoadAverage()%2f)),175,105);
+            g.setFont(new Font("Arial", Font.PLAIN, 16));
+            g.drawString("CPU Count: " + String.valueOf(Runtime.getRuntime().availableProcessors()), 25, 105);
+            g.drawString("Average CPU Usage: " + String.valueOf(MessageFormat.format("{0}%", operatingSystemMXBean.getSystemLoadAverage() % 2f)), 175, 105);
 
             // Disk usage
             File root = new File("/");
-            g.drawString("Total space: "+root.getTotalSpace()/1073741824+" GB",425,105);
-            g.drawString("Free space: "+root.getFreeSpace()/1073741824+" GB",600,105);
-            }
+            g.drawString("Total space: " + root.getTotalSpace() / 1073741824 + " GB", 425, 105);
+            g.drawString("Free space: " + root.getFreeSpace() / 1073741824 + " GB", 600, 105);
         }
-
+    }
     public void tick() {
 
     }
