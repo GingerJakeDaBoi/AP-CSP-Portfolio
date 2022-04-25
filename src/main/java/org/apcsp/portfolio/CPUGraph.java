51f86c7dbf9d575424269591ee1db33b9e26ffbb
GingerJakeDaBoi <GingerJakeDaBoi@users.noreply.github.com>
2022-04-25 16:04:05 -0400

    remove main class from cpugraph because that can cause issues

--- src/main/java/org/apcsp/portfolio/CPUGraph.java
+++ src/main/java/org/apcsp/portfolio/CPUGraph.java
@@ -18,8 +18,8 @@ public class CPUGraph extends JPanel {
     private static final int PREF_W = 800;
     private static final int PREF_H = 650;
     private static final int BORDER_GAP = 30;
-    private static final Color GRAPH_COLOR = Color.green;
-    private static final Color GRAPH_POINT_COLOR = new Color(150, 50, 50, 180);
+    private static final Color GRAPH_COLOR = Color.blue;
+    private static final Color GRAPH_POINT_COLOR = new Color(13, 167, 167, 180);
     private static final Stroke GRAPH_STROKE = new BasicStroke(3f);
     private static final int GRAPH_POINT_WIDTH = 12;
     private static final int Y_HATCH_CNT = 10;
@@ -31,7 +31,7 @@ public class CPUGraph extends JPanel {
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         double xScale = ((double) getWidth() - 2 * BORDER_GAP) / (scores.size() - 1);
@@ -81,7 +81,8 @@ public class CPUGraph extends JPanel {
         g2.setColor(GRAPH_POINT_COLOR);
         for (int i = 0; i < graphPoints.size(); i++) {
             int x = graphPoints.get(i).x - GRAPH_POINT_WIDTH / 2;
-            int y = graphPoints.get(i).y - GRAPH_POINT_WIDTH / 2;;
+            int y = graphPoints.get(i).y - GRAPH_POINT_WIDTH / 2;
+            ;
             int ovalW = GRAPH_POINT_WIDTH;
             int ovalH = GRAPH_POINT_WIDTH;
             g2.fillOval(x, y, ovalW, ovalH);
@@ -97,7 +98,7 @@ public class CPUGraph extends JPanel {
         Random random = new Random();
         int maxDataPoints = 16;
         int maxScore = 20;
-        for (int i = 0; i < maxDataPoints ; i++) {
+        for (int i = 0; i < maxDataPoints; i++) {
             scores.add(random.nextInt(maxScore));
         }
 
@@ -106,13 +107,5 @@ public class CPUGraph extends JPanel {
         frame.pack();
         frame.setLocationByPlatform(true);
         frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGui();
-            }
-        });
     }
 }
