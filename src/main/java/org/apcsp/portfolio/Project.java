65bca612d95c57fa51bfd7fbeb6386717a587d3f
GingerJake <uniquereddy90@gmail.com>
2022-04-27 07:59:13 -0400

    code cleanup and improvements

--- src/main/java/org/apcsp/portfolio/Project.java
+++ src/main/java/org/apcsp/portfolio/Project.java
@@ -1,11 +1,10 @@
 package org.apcsp.portfolio;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.Objects;
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
@@ -14,7 +13,7 @@ import javax.swing.tree.TreePath;
 
 public class Project extends JPanel {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // Create and set up the window.
         JFrame ManagerWindow = new JFrame("Manager");
         ManagerWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -33,22 +32,34 @@ public class Project extends JPanel {
         JPanel panel = new JPanel();
         final JTree browser = new JTree();
         panel.add(new JScrollPane(browser));
-        JButton btn = new JButton("Extend all");
+        JButton btn = new JButton("Expand all");
         btn.addActionListener(ae -> {
-            for (Enumeration e = ((TreeNode)browser.getModel().getRoot()).children(); e.hasMoreElements();) {
+            for (Enumeration<? extends TreeNode> e = ((TreeNode)browser.getModel().getRoot()).children(); e.hasMoreElements();) {
                 TreeNode tn = (TreeNode)e.nextElement();
                 browser.expandPath(new TreePath(((DefaultTreeModel)browser.getModel()).getPathToRoot(tn)));
             }
         });
 
-        File fileRoot = new File("/"); //TODO replace / with C:\
+        File fileRoot = new File("/");
 
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(fileRoot);
         DefaultTreeModel model = new DefaultTreeModel(root);
 
         File[] subItems = fileRoot.listFiles();
-        for (File file : subItems) {
-            root.add(new DefaultMutableTreeNode(file));
+        for (File file : Objects.requireNonNull(subItems)) {
+
+            if (file.isDirectory()) {
+
+                // generates a node newDir using filename
+                DefaultMutableTreeNode newDir = new DefaultMutableTreeNode(file.getName());
+                // adds a node to the root of the JTree
+                root.add(newDir);
+                model.reload();
+
+            } else {
+                DefaultMutableTreeNode newFile = new DefaultMutableTreeNode(file.getName());
+                root.add(newFile);
+            }
         }
 
         browser.setModel(model);
@@ -60,7 +71,7 @@ public class Project extends JPanel {
         frame.setLocation(100, 100);
         frame.pack();
         frame.setPreferredSize(new Dimension(500, 500));
-        frame.setResizable(true);
+        frame.setResizable(false);
 
         //Button
         JButton fileManager = new JButton("Files");
